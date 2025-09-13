e22cde7d9cec580cf63b265a98303a9967a85b8f
Ludvig <ludvig@clin.au.dk>
2025-09-13 03:41:27 +0200

    different bars

--- src/bin/cfdna.rs
+++ src/bin/cfdna.rs
@@ -231,10 +231,11 @@ mod pretty {
 
         // A simple horizontal rule + title
         let title = "cfDNAlab";
-        let bar = "_".repeat(48); // or just "-".repeat(60) for pure ASCII
+        let bar1 = "_".repeat(48); // or just "-".repeat(60) for pure ASCII
+        let bar2 = "─".repeat(48); // or just "-".repeat(60) for pure ASCII
 
         // Start style, content, then reset
-        format!("\n{accent}{bar}\n\n  {title}\n\n{bar}{accent:#}\n")
+        format!("\n{accent}{bar1}\n\n  {title}\n\n{bar2}{accent:#}\n")
     }
 
     /// Apply the signature to a Command and all its subcommands.
